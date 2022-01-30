471ccd65d4521eeb35dd22b7b5375822c1a43d5e
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2022-01-30 11:23:48 -0500

    Update modal links

--- interactiveHome.js
+++ interactiveHome.js
@@ -3,27 +3,27 @@ let span = document.getElementsByClassName("btn-close")[0];
 let modalText = {
   windows: {
     title: "Windows",
-    body: `<p>Solar panels don’t have to be clunky and intrusive - Solar Window Technologies Inc develops clear glass and plastic solar windows to be used in homes, cars, and commercial businesses. According to their website, this year they’ve achieved a record 14.72% power conversion efficiency (PCE) using industry-standard performance testing. Check them out here!</p><a href="https://www.solarwindow.com/" target="_blank" rel="noopener noreferrer">Check it out!</a>`,
+    body: `<p>Solar panels don’t have to be clunky and intrusive - Solar Window Technologies Inc develops clear glass and plastic solar windows to be used in homes, cars, and commercial businesses. According to their website, this year they’ve achieved a record 14.72% power conversion efficiency (PCE) using industry-standard performance testing.</p><a href="https://www.solarwindow.com/" target="_blank" rel="noopener noreferrer">Check solar windows out here!</a>`,
     clicked: false
   },
   fan: {
     title: "fan",
-    body: `<p>What if you could produce light using plants? You can! Several research teams have created algae-powered lamps, which are completely self-sufficient and have the added benefit of absorbing CO2 from the air. Check out some of the algae lamps in development below!</p><a href="https://sustainable-nano.com/2015/03/03/algae-lamps/" target="_blank" rel="noopener noreferrer">Check it out!</a>`,
+    body: `<p>What if you could produce light using plants? You can! Several research teams have created algae-powered lamps, which are completely self-sufficient and have the added benefit of absorbing CO2 from the air.</p> <a href="https://sustainable-nano.com/2015/03/03/algae-lamps/" target="_blank" rel="noopener noreferrer">Check out some of the algae lamps in development here!</a>`,
     clicked: false
   },
   acUnit: {
-    title: "AC Unit",
-    body: `<p>According to some studies, houseplants not only clean air, but they also improve our mental health. Besides these things, one company is even using plants to power electricity using the microorganisms in soil. Check out this doctor’s article about the mental health benefits of plants and biootech.com’s research and development projects!</p><a href="https://wexnermedical.osu.edu/blog/houseplants-are-good-for-your-mental-health" target="_blank" rel="noopener noreferrer">Check it out!</a>`,
+    title: "Air purifier",
+    body: `<p>According to some studies, houseplants not only clean air, but they also improve our mental health. Besides these things, one company is even using plants to power electricity using the microorganisms in soil.</p> <p>Check out this doctor’s article about the <a href="https://wexnermedical.osu.edu/blog/houseplants-are-good-for-your-mental-health" target="_blank" rel="noopener noreferrer">mental health benefits of plants</a> and <a href="https://www.biootech.com/installations" target="_blank" rel="noopener noreferrer">biootech.com</a>’s research and development projects!</p>`,
     clicked: false
   },
   floor: {
     title: "Floor",
-    body: `<p>Hardwood requires deforestation. Bamboo, on the other hand, is a fast-growing grass that can be harvested many times without harming the plant itself. Check out bamboo flooring options!</p><a href="https://www.greenbuildingsupply.com/All-Products/Flooring-Bamboo" target="_blank" rel="noopener noreferrer">Check it out!</a>`,
+    body: `<p>Hardwood requires deforestation. Bamboo, on the other hand, is a fast-growing grass that can be harvested many times without harming the plant itself.</p> <a href="https://www.greenbuildingsupply.com/All-Products/Flooring-Bamboo" target="_blank" rel="noopener noreferrer">Check out bamboo flooring options here!</a>`,
     clicked: false
   },
   fridge: {
     title: "Fridge",
-    body: `<p>According to the most recent climate report by the IPCC, the largest producer of CO2 emissions is the transportation sector. This includes the food we eat - meaning eating locally is a far more sustainable option. Community Supported Agriculture (CSA) is a fantastic way to support local farms, eat healthier produce, and lower your carbon footprint. Find a CSA near you!</p><a href="https://www.ams.usda.gov/local-food-directories/csas" target="_blank" rel="noopener noreferrer">Check it out!</a>`,
+    body: `<p>According to the most recent climate report by the IPCC, the largest producer of CO2 emissions is the transportation sector. This includes the food we eat - meaning eating locally is a far more sustainable option. Community Supported Agriculture (CSA) is a fantastic way to support local farms, eat healthier produce, and lower your carbon footprint.</p> <a href="https://www.ams.usda.gov/local-food-directories/csas" target="_blank" rel="noopener noreferrer">Find a CSA near you!</a>`,
     clicked: false
   },
   "green-windows": {
