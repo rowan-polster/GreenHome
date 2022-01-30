e5c9ffbbb8c42dd3f38fe3399ac98aebd26ada90
Lesley_Heyer <lesley@wewhistle.com>
2022-01-29 19:26:32 -0600

    replaced penguin-icon image, added body text, adding link text, edited CSS for bootstrap modal

--- interactiveHome.js
+++ interactiveHome.js
@@ -17,11 +17,16 @@ function openModal(e) {
   let clickedId = e.target.id;
 
   let modalText = {
-    windows: { title: "Windows", body: "Windows Lorem ipsum" },
-    wall: { title: "Wall", body: "Wall Lorem ipsum" },
-    acUnit: { title: "AC Unit", body: "AC Lorem ipsum" },
-    floor: { title: "Floor", body: "Floor Lorem ipsum" },
-    fridge: { title: "Fridge", body: "Fridge Lorem ipsum" }
+    windows: { title: "Windows", body: `Solar panels don’t have to be clunky and intrusive - Solar Window Technologies Inc develops clear glass and plastic solar windows to be used in homes, cars, and commercial businesses. According to their website, this year they’ve achieved a record 14.72% power conversion efficiency (PCE) using industry-standard performance testing. Check them out here!`,
+    link: "https://www.solarwindow.com/" },
+    fan: { title: "fan", body: `What if you could produce light using plants? You can! Several research teams have created algae-powered lamps, which are completely self-sufficient and have the added benefit of absorbing CO2 from the air. Check out some of the algae lamps in development below!`,
+    link: "https://sustainable-nano.com/2015/03/03/algae-lamps/" },
+    acUnit: { title: "AC Unit", body: `According to some studies, houseplants not only clean air, but they also improve our mental health. Besides these things, one company is even using plants to power electricity using the microorganisms in soil. Check out this doctor’s article about the mental health benefits of plants and biootech.com’s research and development projects!`,
+    link: "https://wexnermedical.osu.edu/blog/houseplants-are-good-for-your-mental-health" },
+    floor: { title: "Floor", body: `Hardwood requires deforestation. Bamboo, on the other hand, is a fast-growing grass that can be harvested many times without harming the plant itself. Check out bamboo flooring options!`,
+    link: "https://www.greenbuildingsupply.com/All-Products/Flooring-Bamboo" },
+    fridge: { title: "Fridge", body: `According to the most recent climate report by the IPCC, the largest producer of CO2 emissions is the transportation sector. This includes the food we eat - meaning eating locally is a far more sustainable option. Community Supported Agriculture (CSA) is a fantastic way to support local farms, eat healthier produce, and lower your carbon footprint. Find a CSA near you!`,
+    link: "https://www.ams.usda.gov/local-food-directories/csas" }
   };
 
   //Switch out modal's head and body text
@@ -29,6 +34,8 @@ function openModal(e) {
   title.innerHTML = modalText[clickedId].title;
   let body = document.getElementsByClassName("modal-body")[0];
   body.innerHTML = modalText[clickedId].body;
+  // let link = document.getElementsByClassName("modal-link")[0];
+  // body.innerHTML = modalText[clickedId].link;
 
   // When the user clicks the button, open the modal
   modal.style.display = "block";
